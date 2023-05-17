85a0d0358997551ddfc6cf717ebeadb3cb140748
manyayadav <94862658+manyayadav@users.noreply.github.com>
2023-05-17 20:55:52 +0530

    Create loginscreen.java

---
+++ loginscreen.java
@@ -0,0 +1,14 @@
+package com.example.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginButton;
