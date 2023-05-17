ea0b756786176f9fdc1346b77df9ac169b96c3d6
manyayadav <94862658+manyayadav@users.noreply.github.com>
2023-05-17 20:59:36 +0530

    Create makeText.java

---
+++ makeText.java
@@ -0,0 +1,12 @@
+ loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
