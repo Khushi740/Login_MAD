2a962a10fbd0e4fa89948a87500b4495d50b7443
manyayadav <94862658+manyayadav@users.noreply.github.com>
2023-05-17 20:58:24 +0530

    Create onCreate.java

---
+++ onCreate.java
@@ -0,0 +1,7 @@
+ @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
